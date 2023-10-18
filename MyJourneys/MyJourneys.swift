cfc0bd5c3b43e1c2583680539de7e64b9470f5c0
LATIFARYES <latifaryes@gmail.com>
2023-10-19 01:52:11 +0300

    add photos

---
+++ MyJourneys/MyJourneys.swift
@@ -0,0 +1,18 @@
+//
+//  MyJourneys.swift
+//  MyJourneys
+//
+//  Created by LATIFA on 19/10/2023.
+//
+
+import SwiftUI
+
+struct MyJourneys: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MyJourneys()
+}
