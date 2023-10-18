4d8fc44682271507c1a40730ff0565b57e83ee54
LATIFARYES <latifaryes@gmail.com>
2023-10-19 01:49:40 +0300

    Initial Commit

---
+++ MyJourneys/MyJourneysApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyJourneysApp.swift
+//  MyJourneys
+//
+//  Created by LATIFA on 19/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct MyJourneysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
